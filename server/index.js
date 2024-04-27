c6fc8f879587dad6156c3e06620c345c032bde4a
Ashraf22 <ashrafmaged593@gmail.com>
2024-04-27 12:28:38 +0300

    same app but using express and front and back are running on the same server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
-import { createServer } from "http";
+import express from "express";
 import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const httpServer = createServer();
-const io = new Server(httpServer, {
+const PORT = process.env.PORT || 3500;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const expressServer = app.listen(PORT, () => {
+  console.log(`ok listinging on port ${PORT}`);
+});
+
+const io = new Server(expressServer, {
   cors: {
     origin:
       process.env.NODE_ENV === "production"
@@ -10,14 +21,10 @@ const io = new Server(httpServer, {
         : ["http://127.0.0.1:5500", "http://localhost:5500"],
   },
 });
+
 io.on("connection", (socket) => {
   console.log(`user ${socket.id} connected!`);
   socket.on("message", (data) => {
-    // console.log(data);
     io.emit("message", `${socket.id.substring(0, 5)}: ${data}`);
   });
-});
-
-httpServer.listen(3500, () => {
-  console.log("ok listinging on port 3500");
 });
